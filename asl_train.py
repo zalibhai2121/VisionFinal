1d0a1c344db91ebfb96b8d77ca525e5bb40b39d4
Michael Andaloro <andaloromichael@gmail.com>
2020-04-26 12:46:45 -0700

    Continue work on asl_train; saving work

--- asl_train.py
+++ asl_train.py
@@ -4,6 +4,7 @@ import numpy as np
 import cv2
 import tensorflow as tf
 import os
+import time
 
 # Remove any TF log outputs (e.g. CPU supporting stuff)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -37,8 +38,53 @@ def build_asl_model():
                   metrics=['accuracy'])
     print("Done building the network topology.")
 
-    # Read training and prediction data
+    # Read training and prediction data and save as a checkpoint
+    train, train_labels, predict, predict_labels = read_labels_and_images()
+    checkpoint = tf.keras.callbacks.ModelCheckpoint("models/fingersteps/checkpoint_{epoch}")
 
+def read_labels_and_images():
+    start_time = time.time()
+    filepath = "dataset/"
+    # Read the label file, mapping image filenames to (x, y) pairs for UL corner
+    with open(filepath + "labels") as label_file:
+        label_string = label_file.readline()
+        label_map = eval(label_string)
+    print("Read label map with", len(label_map), "entries")
+
+    # Read the directory and make a list of files
+    filenames = []
+    for filename in os.listdir(filepath):
+        if random.random() < -0.7: continue # to select just some files
+        if filename.find("jpg") == -1: continue # process only images
+        filenames.append(filename)
+    print("Read", len(filenames), "images.")
+
+    # Extract the features from the images
+    print("Extracting features")
+    train, train_labels, predict, predict_labels = [], [], [], []
+    processes = []     # Hold (process, queue) pairs
+    num_processes = 8  # Process files in parallel
+
+    # Launch the processes
+    for start in range(num_processes):
+        q = Queue()
+        files_for_one_process = filenames[start:len(filenames):num_processes]
+        file_process = Process(target=process_files, \
+                        args=(filepath, files_for_one_process, label_map, q))
+        processes.append((file_process, q))
+        file_process.start()
+
+    # Wait for processes to finish, combine their results
+    for p, q in processes: # For each process and its associated queue
+        result = q.get()            # Blocks until the item is ready
+        train += result[0]          # Get training features from child
+        train_labels += result[1]   # Get training labels from child
+        predict += result[2]        # Get prediction features from child
+        predict_labels += result[3] # Get prediction labels from child
+        p.join()                    # Wait for child process to finish
+    print("Done extracting features from images. Time =", time.time() - start_time)
+
+    return (train, train_labels, predict, predict_labels)
 
 
 build_asl_model()
