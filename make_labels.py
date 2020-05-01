6240168510c8f1abb28866f58e5f2c155b479d8f
gabriellasanford <37630114+gabriellasanford@users.noreply.github.com>
2020-05-01 16:30:48 -0500

    Learn to compress your damn files
    also I'm fixing the comments and spacing to more proper since PyCharm is taking forever to load

--- make_labels.py
+++ make_labels.py
@@ -1,10 +1,3 @@
-"""
-This program iterates over the dataset of hands and creates a dictionary
-of key value pairs consisting of the file name and the letter it represents
-
-After making the dictionary, it saves it as a .txt file for the NN to use
-"""
-
 import os
 from typing import Dict
 
@@ -12,12 +5,20 @@ image_directory: str = "dataset"
 label_file: str = "labels.txt"
 
 def make_labels():
+    """
+    This method iterates over the dataset of sign language images,
+    creating a dictionary of key value pairs consisting of the file name
+    and the letter it represents.
+    
+    after making the dictionary, it saves it as a .txt file for the NN to use
+    """
     labels = dict()
+    
     for filename in os.listdir(image_directory):
         current_filename = filename
         letter = current_filename[:1]
         labels.update({current_filename: letter})
-    #os.remove("dataset/labels.txt")
+    
     file = open("dataset/labels.txt", "w")
     file.write(str(labels))
     file.close()
