6c154fc7e32796f9239257a997df02e63398ab56
Michael Andaloro <andaloromichael@gmail.com>
2020-05-01 15:42:27 -0700

    Made first move_five, make_labels was correct

--- make_labels.py
+++ make_labels.py
@@ -1,7 +1,7 @@
 import os
 from typing import Dict
 
-image_directory: str = "extra_dataset"
+image_directory: str = "dataset"
 label_file: str = "labels.txt"
 
 def make_labels():
@@ -9,16 +9,16 @@ def make_labels():
     This method iterates over the dataset of sign language images,
     creating a dictionary of key value pairs consisting of the file name
     and the letter it represents.
-    
+
     after making the dictionary, it saves it as a .txt file for the NN to use
     """
     labels = dict()
-    
+
     for filename in os.listdir(image_directory):
         current_filename = filename
         letter = current_filename[:1]
         labels.update({current_filename: letter})
-    
+
     file = open("dataset/labels.txt", "w")
     file.write(str(labels))
     file.close()
