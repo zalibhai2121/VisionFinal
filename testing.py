d2378a3c98dcbddfd2bee55bdac4132a26ba5981
Michael Andaloro <andaloromichael@gmail.com>
2020-05-02 18:29:08 -0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 # Build the CNN
+# Takes nothing as input, returns a model
 def build_cnn():
     model = tf.keras.Sequential()
     # Convolutional Layer
@@ -13,6 +14,24 @@ def build_cnn():
     # Pooling
     model.add(tf.keras.layers.MaxPool2D(pool_size=(2,2)))
     # MORE LAYERS
-    model.add()
+    model.add(tf.keras.layers.Conv2D(64, (3, 3)))
+    model.add(tf.keras.layers.Activation("relu"))
+    model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
+    # Flatten and Dense the layers
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(64))
+    model.add(tf.keras.layers.Dense(1))
+    model.add(tf.keras.layers.Activation("sigmoid"))
+    # Compile the model
+    model.compile(loss = "binary_crossentropy", # Maybe categorical_crossentropy?
+                  optimizer = "adam",
+                  metrix = ['accuracy'])
+
+    print("Finished building the CNN")
+    return model
+
+# Fit the model to the images
+def fit_cnn():
+    model
 
 build_cnn()
