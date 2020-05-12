b5a7bd7e3720a4c809e65a99f40e029f9f58c0e0
Michael Andaloro <andaloromichael@gmail.com>
2020-05-11 23:19:45 -0700

    Add rest of sources

--- sources.py
+++ sources.py
@@ -5,10 +5,19 @@ Here are the following websites and repos that we got help from:
 2) Dataset: https://github.com/loicmarie/sign-language-alphabet-recognizer
 3) Prevent tf log messages from appearing: https://github.com/tensorflow/tensorflow/issues/27023
 4) Changing the images: https://stackoverflow.com/questions/10759117/converting-jpg-images-to-png
-5) Resizing the images fro loicmarie's repo from 200x200 to 64x64: https://stackoverflow.com/questions/21517879/python-pil-resize-all-images-in-a-folder
+5) Resizing the images from loicmarie's repo from 200x200 to 64x64: https://stackoverflow.com/questions/21517879/python-pil-resize-all-images-in-a-folder
 6) OpenCV to make the hand white and the background black: https://github.com/harshbg/Sign-Language-Interpreter-using-Deep-Learning/tree/master/Code
 ^ Still needs to be used, I haven't gotten around to training with black and white hands...
 7) General learning guide: https://machinelearningmastery.com/how-to-develop-a-convolutional-neural-network-from-scratch-for-mnist-handwritten-digit-classification/
 8) Converting numbers to letters in python: https://stackoverflow.com/questions/4528982/convert-alphabet-letters-to-number-in-python
 9) Putting text on the frame: https://www.geeksforgeeks.org/python-opencv-cv2-puttext-method/
+10) General: https://github.com/abdulhaim/Sign-Language-Interpreter
+11) TF help: https://www.tensorflow.org/tutorials/load_data/numpy
+12) Dataset: https://www.kaggle.com/datamunge/sign-language-mnist
+13) Didn't use in code but helped: https://github.com/Arshad221b/Sign-Language-Recognition-
+14) https://github.com/ayberktecimer/Sign_Language_Detector
+15) https://www.geeksforgeeks.org/python-pil-image-resize-method/
+16) https://stackoverflow.com/questions/2632205/how-to-count-the-number-of-files-in-a-directory-using-python
+17) https://keras.io/api/preprocessing/image/
+18) https://github.com/keras-team/keras/issues/3946
 """
